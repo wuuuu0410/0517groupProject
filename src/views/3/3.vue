2ebd92343cf354d99e9a7b1a82e71a136505c680
wuuuu0410 <wu870410@mail.com>
2024-05-11 09:38:14 +0800

    add select分類

--- src/views/3/3.vue
+++ src/views/3/3.vue
@@ -3,54 +3,55 @@ export default {
     data() {
         return {
             //taipeiRestaurantsList收集臺北市餐廳
-            taipeiRestaurantsList:[],
+            taipeiRestaurantsList: [],
             //town收集台北市行政區
-            town:[],
+            town: [],
             //selectList收集與select.value相符的餐廳
-            selectList:[],
-            selectValue:"",
+            selectList: [],
+            selectValue: "",
         };
     },
     created() {
         this.getTaipeiRestaurants();  //從全台灣餐廳過濾出臺北市餐廳
     },
-    mounted(){
-        this.getTown();
-    },
+
     methods: {
-    async getTaipeiRestaurants() {
+        //篩選出台北餐廳後執行getTown()
+        async getTaipeiRestaurants() {
             try {
                 const response = await fetch('../RestaurantList.json');
-            if (!response.ok) {
-                throw new Error('Failed to fetch data');
-            }
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
                 const data = await response.json();
                 console.log(data)
-                for(let i = 0; i<data.Restaurants.length;i++){
-                    if(data.Restaurants[i].PostalAddress.City=="臺北市"){
+                for (let i = 0; i < data.Restaurants.length; i++) {
+                    if (data.Restaurants[i].PostalAddress.City == "臺北市") {
                         this.taipeiRestaurantsList.push(data.Restaurants[i]);
-                    } 
+                    }
                 }
                 console.log(this.taipeiRestaurantsList);
+                this.getTown();
             } catch (error) {
                 console.error(error);
             }
         },
-        getTown(){
-            for(let i=0; i<this.taipeiRestaurantsList.length; i++){
+        //取得行政區
+        getTown() {
+            for (let i = 0; i < this.taipeiRestaurantsList.length; i++) {
                 this.town.push(this.taipeiRestaurantsList[i].PostalAddress.Town)
-                }
+            }
             const set = new Set();
             this.town = this.town.filter(item => !set.has(item) ? set.add(item) : false);
             console.log(this.town);
         },
         //比對select選取的value,並收集相同行政區的餐廳
-        checkSelect(){
+        checkSelect() {
             const select = document.querySelector("#select")
-            this.selectValue=select.value;
+            this.selectValue = select.value;
             this.selectList = [];
-            for(let i=0; i<this.taipeiRestaurantsList.length;i++){
-                if(this.taipeiRestaurantsList[i].PostalAddress.Town==this.selectValue){
+            for (let i = 0; i < this.taipeiRestaurantsList.length; i++) {
+                if (this.taipeiRestaurantsList[i].PostalAddress.Town == this.selectValue) {
                     this.selectList.push(this.taipeiRestaurantsList[i])
                 }
             }
@@ -63,23 +64,83 @@ export default {
 
 
 <template>
-
-    <input type="text">
+    <div class="headerCarousel"></div>
+    <div class="select">
     <select @change="checkSelect" name="" id="select">
         <option value="">請選擇區域</option>
         <option v-for="item in this.town" v-bind:value="item">{{ item }}</option>
-    </select> 
-    <button type="button">搜尋</button>
-    <div class="a">
-    <h2>{{ taipeiRestaurantsList[1].RestaurantName }}</h2>
-    <ul v-for="item in selectList">{{ item.RestaurantName }} </ul>
+    </select>
     </div>
-    
+
+    <div class="list" v-for="item in selectList">
+        <div class="listImages">
+            <img v-if="item.Images.length>=1" v-bind:src="item.Images[0].URL" alt="" width="100px" height="100px">
+            <img v-if="item.Images.length>=2" v-bind:src="item.Images[1].URL" alt="" width="100px" height="100px">
+            <img v-if="item.Images.length>=3" v-bind:src="item.Images[2].URL" alt="" width="100px" height="100px">
+        </div>
+        <div class="ulSpace">
+            <ul class="restName" >
+                <li><h2>{{ item.RestaurantName }}</h2></li>
+                <li>{{ item.PostalAddress.StreetAddress }}</li>
+                <li>{{ item.ServiceTimeInfo }}</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="card">
+        
+    </div>
+
 </template>
 
 
 
 
 <style>
+.headerCarousel{
+    width: 100%;
+    height: 80dvh;
+    border: 1px solid black;
+}
 
+.select{
+    width: 100%;
+    height: 10dvh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+#select{
+    width: 25%;
+    height: 5dvh;
+    background: black;
+    color: white;
+    font-size: 25px;
+    text-align: center;
+}
+.list{
+    width: 70%;
+    margin: 0 auto;
+    /* background-image: url(../public/紙.jpg); */
+    background-repeat: no-repeat;
+    background-size: cover;
+    border: 1px solid black;
+    display: flex;
+    
+}
+.restName{
+    list-style: none;
+
+}
+.listImages{
+    width: 20%;
+    height: 300px;
+    left: 0;
+    flex-wrap: wrap;
+}
+.ulSpace{
+    width: 80%;
+    height: 300px;
+}
 </style>
