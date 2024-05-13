ea5dc286899638bd6d2c586fd7cce6dd762df612
Tsai Hong-Ping <t0736828@gmail.com>
2024-05-13 09:43:44 +0800

    add fetchdata.js into 2

--- src/views/2/2.vue
+++ src/views/2/2.vue
@@ -1,5 +1,20 @@
 <template>
   <h1>這裡是住宿區</h1>
+  <label>請選擇城市：</label>
+  <select>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+    <option value="">城市</option>
+  </select>
 </template>
 
 <style scoped lang="scss">
