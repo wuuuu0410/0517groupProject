8aa4d5778572f0042a11b4a21f0bc84d0065a4f6
Tsai Hong-Ping <t0736828@gmail.com>
2024-05-13 17:10:02 +0800

    加按鈕跟核選方塊至2.vue

--- src/views/2/2.vue
+++ src/views/2/2.vue
@@ -6,7 +6,9 @@ export default {
   data() {
     return {
       accessToken: null,
-      apiResponse: null
+      apiResponse: null,
+      filterData:null,
+      cityName:"",
     }
   },
   // 建立生命週期函式
@@ -75,11 +77,55 @@ export default {
 
 <template>
   <div>
-    <h3>取得憑證資料:</h3>
-    <pre>{{ accessToken }}</pre>
-
-    <h3>api 資料:</h3>
-    <pre>{{ apiResponse }}</pre>
+      <div>
+        <label for="cityname">你選擇的縣市：{{cityName}}</label>
+        <select name="cityname" id="cityname" v-model="cityName" required>
+          <option>請選擇</option>
+          <option value="Taipei">臺北市</option>
+          <option value="NewTaipei">新北市</option>
+          <option value="Taoyuan">桃園市</option>
+          <option value="Taichung">台中市</option>
+          <option value="Tainan">台南市</option>
+          <option value="Kaohsiung">高雄市</option>
+          <option value="Keelung">基隆市</option>
+          <option value="Hsinchu">新竹縣</option>
+          <option value="HsinchuCounty">新竹市</option>
+          <option value="MiaoliCounty">苗栗縣</option>
+          <option value="NantouCounty">南投縣</option>
+          <option value="ChanghuaCounty">彰化縣</option>
+          <option value="YunlinCounty">雲林縣</option>
+          <option value="ChiayiCounty">嘉義縣</option>
+          <option value="Chiayi">嘉義市</option>
+          <option value="PingtungCounty">屏東縣</option>
+          <option value="YilanCounty">宜蘭縣</option>
+          <option value="HualienCounty">花蓮縣</option>
+          <option value="TaitungCounty">臺東縣</option>
+          <option value="PenghuCounty">澎湖縣</option>
+          <option value="KinmenCounty">金門縣</option>
+          <option value="LienchiangCounty">連江縣</option>
+        </select>  
+      </div>
+      <div>
+        <input type="checkbox" name="" id=""> 年度活動
+        <input type="checkbox" name="" id=""> 節慶活動
+        <input type="checkbox" name="" id=""> 藝文活動
+      </div>
+    <button>搜尋</button>
+    <!--this is for data test-->
+      <!-- <diV> -->
+        <h3>api 資料:</h3>
+        <pre>{{ apiResponse }}</pre>
+      <!-- </diV> -->
+    <!--this is for data test-->
+    <div v-for="item in apiResponse" :key="item">
+      <h1>活動名稱：{{ item.ActivityName }}</h1>
+      <p>活動描述：{{item.Description}}</p>
+      <p>主辦單位：{{item.Organizer}}</p>
+      <p>活動時間：{{item.StartTime.split('T')[0]}} - {{item.EndTime.split('T')[0]}}</p>
+      <div>
+      <p v-if="item.Class1 !== ''">活動類型：{{item.Class1}}</p>
+    </div>
+    </div>
   </div>
 
 </template>
